5c46f7f982bb8d4967eaab815b6880da6a3e4648
Thomas Stroemme <strtho@gmail.com>
2013-03-05 15:08:57 +0100

    Cleaning up code

--- barcode.rb
+++ barcode.rb
@@ -14,10 +14,8 @@ def make_random_barcode(number_of_times)
   ean_digits = 100000000000
   file_format = ".png"
   
-  if directory_exists?(dir)
-  
+  if directory_exists?(dir)  
     rbn = number_of_times.times.map { ean_digits + Random.rand(1000)  }
-  
     rbn.each do |r|
       barcode = Barby::EAN13.new(r.to_s)
       File.open(dir+"#{r}#{barcode.checksum}"+file_format, "w") { |file| file.write barcode.to_png }
@@ -27,10 +25,4 @@ def make_random_barcode(number_of_times)
   end
 end
 
-def barcode_number
-  barcode = Barby::EAN13.new("100000000000")
-  puts barcode.checksum
-end
-
-#barcode_number
 make_random_barcode(2)
